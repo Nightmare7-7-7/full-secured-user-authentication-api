f8fa1cded947762388af961ac78db79f4caf7171
khumanpoudel <khumanpoudel124421@gmail.com>
2026-01-31 16:02:57 +0000

    feat: add user verification middleware and extend Express Request type

---
+++ src/middlewares/verfyUser.middleware.ts
@@ -0,0 +1,40 @@
+import { User } from './../../generated/prisma/index.d';
+import { NextFunction, Request, Response } from "express";
+import { verifyToken } from "../utils/jwt_token";
+import { object } from 'zod';
+
+
+
+const verfyUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const token = req.headers["auth_token"] || req.headers["authorization"];
+
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized"
+            });
+        }
+
+        const decoded = await verifyToken(token as string);
+
+        if (!decoded) {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized"
+            });
+        }
+
+        req.user = decoded;
+        next();
+
+    }
+    catch (err) {
+        return res.status(400).json({
+            success: false,
+            message: "Malformed Token"
+        })
+    }
+}
+
+export default verfyUser;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,18 @@
 import { createAccount, Login } from "../controllers/auth.controller";
+import verfyUser from "../middlewares/verfyUser.middleware";
 import express from "express";
 
 const route = express.Router();
 
 route.post("/create", createAccount);
 route.post("/login", Login);
+route.get("/me",verfyUser, (req, res)=>{
+    return res.status(200).json({
+        success: true,
+        message: "Hello authenticated user",
+        data: req.user
+    })
+});
+
+
 export default route;
