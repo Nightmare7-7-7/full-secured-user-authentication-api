bfc7f647ca0f246d3ceb78af9ab594788a7d6eb9
khumanpoudel <khumanpoudel124421@gmail.com>
2026-02-01 07:44:32 +0000

    feat: add express-rate-limit middleware for request rate limiting

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,27 @@ import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.routes";
 import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 //load env variables
 dotenv.config();
 
+//global rate limiter
+export const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // 100 requests per IP
+  standardHeaders: true, // send rate limit info in headers
+  legacyHeaders: false,
+});
+
+
 const app = express();
 app.use(express.json());
 app.use(helmet());
+app.use(globalLimiter);
 
 
 const PORT = Number(process.env.PORT) || 8181;
-
 
 //start server
 app.listen(PORT, () => {
